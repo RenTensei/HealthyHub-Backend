c5f8233b3352f63663669e67ef88545797508cfb
aggenov <a.g.genov@gmail.com>
2023-09-19 20:56:09 +0300

    add getDiaryFood

--- src/controllers/foodIntakeController.js
+++ src/controllers/foodIntakeController.js
@@ -4,10 +4,10 @@ const { SaveFoodIntakeValidationSchema } = require('../models/FoodIntake/FoodInt
 const FoodIntakeModel = require('../models/FoodIntake/FoodIntakeModel');
 const FoodAllModel = require('../models/FoodIntake/RecomendedFoodModel');
 
-const getAll = async (req, res) => {
-  const foodIntakes = await FoodIntakeModel.find({ consumer: req.user._id });
-  res.json(foodIntakes);
-};
+// const getAll = async (req, res) => {
+//   const foodIntakes = await FoodIntakeModel.find({ consumer: req.user._id });
+//   res.json(foodIntakes);
+// };
 
 const create = async (req, res) => {
   const validatedBody = SaveFoodIntakeValidationSchema.parse(req.body);
@@ -21,25 +21,31 @@ const create = async (req, res) => {
 };
 
 const getDiaryFood = async (req, res) => {
-  console.log(req.query.date);
+  const currentData = new Date();
+  const todayStart = new Date(new Date().setHours(0, 0, 0, 0));
+  // const todayEnd = new Date(new Date().setHours(23, 59, 59, 999));
+
   const result = await FoodIntakeModel.find({
+    consumer: req.user._id,
     createdAt: {
-      // $gte: req.query.date,
-      $lte: req.query.date,
+      $gte: todayStart,
+      $lte: currentData,
+      // $lte: todayEnd,
     },
   }).exec();
 
   res.json(result);
 };
-const getAllFood = async (req, res) => {
+
+const getRecommendedFood = async (req, res) => {
   const result = await FoodAllModel.find({}, { _id: 0 });
 
   res.json(result);
 };
 
 module.exports = {
-  getAll: handlerWrapper(getAll),
+  // getAll: handlerWrapper(getAll),
   create: handlerWrapper(create),
   getDiaryFood: handlerWrapper(getDiaryFood),
-  getAllFood: handlerWrapper(getAllFood),
+  getRecommendedFood: handlerWrapper(getRecommendedFood),
 };
